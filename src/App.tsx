15267208848ad4354737f58aed8a0b460eabc2d1
Dania Jawad <djawad02@gmail.com>
2024-08-08 13:07:42 +0500

    Getting Started with Zustand - counter component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
 import { AuthProvider } from "./state-management/auth";
 import { TaskProvider } from "./state-management/tasks";
+import Counter from "./state-management/counter/Counter";
 
 function App() {
   return (
     <AuthProvider>
       <TaskProvider>
+        <Counter />
         <NavBar />
         <HomePage />
       </TaskProvider>
